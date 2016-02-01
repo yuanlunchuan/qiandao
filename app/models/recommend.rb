46e8516ce493c91be40309c9775a06d85c7350e3
Yuan Lunchuan <799652013@qq.com>
2016-02-01 16:02:13 +0800

    1, Add phone_number validates.

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,9 +1,8 @@
 class Recommend < ActiveRecord::Base
   belongs_to :event
-
+  validates :phone_number, presence: true, length: { is: 11 }, numericality: { integer_only: true }, format: { with: /\A1[3-9]\d{9}\z/ }
   validates :category_name, presence: true
   validates :recommend_name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
   validates :distance, presence: true
 end
