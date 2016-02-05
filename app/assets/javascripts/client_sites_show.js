7d180d867389a1f714efd8cbaf014b2fe25e7657
Qin Junce <qinjunce@xhjdcd.com>
2016-02-05 11:24:53 +0800

    首页自适应修改

--- app/assets/javascripts/client_sites_show.js
+++ app/assets/javascripts/client_sites_show.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   if($('.illus-area').height()== 1)//网路差图片还没加载出来
   {
-    var img_height = $('.illus-area').height();
+    var img_height = parseInt(($(window).width()/496)*228);//计算图片的高度
     var height = $(window).height()-$('.all-page').height()-img_height;
   }
   else
