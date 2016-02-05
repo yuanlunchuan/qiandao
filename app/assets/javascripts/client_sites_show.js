309cfd9d11b6f4213270be20bc11fde55ef4915c
Qin Junce <qinjunce@xhjdcd.com>
2016-02-05 11:11:15 +0800

    首页自适应能力修复

--- app/assets/javascripts/client_sites_show.js
+++ app/assets/javascripts/client_sites_show.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-  //计算表格应该自适应的高度
-  var height = $(window).height()-$('.all-page').height();
-    //设置每一行的高度
-  alert("屏幕高度:"+$(window).height());
-  alert("其他部分高度:"+$('.all-page').height());
-  alert("实际的高度:"+(height-1)/2);
-  $('.function-area tr').height((height-5)/2);
+
+  if($('.illus-area').height()== 1)//网路差图片还没加载出来
+  {
+    var img_height = $('.illus-area').height();
+    var height = $(window).height()-$('.all-page').height()-img_height;
+  }
+  else
+  {
+    var height = $(window).height()-$('.all-page').height();
+  }
+  //设置每一行的高度
+  $('.function-area tr').height((height-1)/2);
 })
