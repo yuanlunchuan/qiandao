88ec07c9e1095c4c45ddc503b771aebc66a6f7b0
Yuan Lunchuan <799652013@qq.com>
2016-02-07 21:52:27 +0800

    1, Add invitation page.

--- app/controllers/invitation_settings_controller.rb
+++ app/controllers/invitation_settings_controller.rb
@@ -4,14 +4,19 @@ class InvitationSettingsController < ApplicationController
   layout 'event'
 
   def index
+    @template = SmsTemplate.find_or_create_by(event: current_event)
+    @sms_template = @template.content
     @settings = InvitationSetting.find_or_create_by(event: current_event)
     @current_module = 1
   end
 
   def update
     @current_module = 1
+
+    @template = SmsTemplate.find_or_create_by(event: current_event)
+
     @settings = current_event.invitation_setting
-    if @settings.update(settings_param)
+    if @settings.update(settings_param)&&@template.update(content: params[:sms_template])
       redirect_to event_invitation_settings_path, flash: {success: '保存成功'}
     else
       flash.now[:error] = @settings.errors.full_messages
