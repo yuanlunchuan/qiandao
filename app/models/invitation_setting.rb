fdffb832e69df5f2b5167b9427fd658ae5764327
Yuan Lunchuan <799652013@qq.com>
2016-02-07 10:51:34 +0800

    1, Add phto.

--- app/models/invitation_setting.rb
+++ app/models/invitation_setting.rb
@@ -1,3 +1,8 @@
 class InvitationSetting < ActiveRecord::Base
   belongs_to :event
+  has_attached_file :photo,
+                    styles: { medium: "300x300>", square:'300x300#', thumb: "100x100>", large: '1000x1000>'},
+                    url: '/system/events/:event_id/attendees/:style/:attendee_number-:token.jpg',
+                    default_url: 'avatar.png'
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
