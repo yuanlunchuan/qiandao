3709209f286c01edd4b58e22af104e01c1ceb010
Yuan Lunchuan <799652013@qq.com>
2016-02-08 15:07:49 +0800

    1, Add monitor.

---
+++ app/controllers/checkin_monitors_controller.rb
@@ -0,0 +1,43 @@
+class CheckinMonitorsController < ApplicationController
+  before_action :authorize_admin!
+  before_action :set_current_module
+
+  layout 'event'
+
+  def index
+    @sessions = current_event.sessions
+  end
+
+  def edit
+    @session = current_event.sessions.find(params[:id])
+  end
+
+  def update
+    @session = current_event.sessions.find(params[:id])
+
+    if @session.update(session_params)
+      redirect_to event_checkin_monitors_path, flash: {success: '保存成功'}
+    else
+      flash.now[:error] = @session.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @session = current_event.sessions.find(params[:id])
+    @session.destroy
+
+    redirect_to event_checkin_monitors_path(current_event)
+  end
+
+  def set_current_module
+    @current_module = 4
+  end
+
+  def session_params
+    params.require(:session).permit(:name, :desc, :start_time, :end_time, :start_date, :checkin_enabled, :company_checkin, :hidden, :question_enabled)
+  end
+
+  private :set_current_module, :session_params
+
+end
