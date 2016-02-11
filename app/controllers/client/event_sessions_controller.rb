7cf64956448b8131d7171235f9daea4a9ce0d250
Yuan Lunchuan <799652013@qq.com>
2016-02-11 14:12:42 +0800

    1, Add session.

--- app/controllers/client/event_sessions_controller.rb
+++ app/controllers/client/event_sessions_controller.rb
@@ -8,6 +8,7 @@ class Client::EventSessionsController < ApplicationController
 
   def index
     self.meta = params
+
     if params[:format]=='json'
       event = Event.find_by(id: params[:event_id])
       render_not_found '活动id不存在' and return if event.blank?
@@ -38,29 +39,8 @@ class Client::EventSessionsController < ApplicationController
           end
         end
       end
-      @collection << item if item.present? 
+      @collection << item if item.present?
     end
-
-    if params[:format]=='json'
-      render_ok @collection
-    end
-
-
-=begin    
-    self.meta = params
-    event = Event.find_by(id: params[:event_id])
-    render_not_found '会议日程为空' and return if event.blank?
-
-    @sessions = event.sessions
-    collection = []
-
-    @sessions.each do |session|
-      collection << session.to_hash
-    end
-
-    render_ok collection and return if collection.present?
-    render_not_found '会议日程为空'
-=end
   end
 
   def show
@@ -85,69 +65,6 @@ class Client::EventSessionsController < ApplicationController
       end
       @collection << item if item.present? 
     end
-
-    if params[:format]=='json'
-      render_ok @collection
-    end
-
-
-=begin
-    collection = []
-    current_event.sessions.each do |session|
-      start_date = session.start_date
-      has_find = false
-      item = {}
-
-      collection.each do |collection_item|
-        if collection_item[:start_date] == start_date
-          has_find = true
-          if item[:collection].blank?
-            item[:collection] = []
-          end
-          item[:collection] << session
-        end
-      end
-
-      if !has_find
-        item[:start_date] = start_date
-        item[:collection] = []
-        item[:collection] << session
-      end
-
-      collection << item
-      logger.info "---------collection: #{collection}"
-    end
-    logger.info "++++++++++collection+++++++: #{collection}"
-=end
-
-=begin
-    current_event.sessions.each do |session|
-      starts_at = (session.starts_at-8*60*60).strftime('%Y%m%d').to_i
-      has_find = false
-      item = {}
-
-      collection.each do |collection_item|
-        if collection_item[:starts_at] == starts_at
-          has_find = true
-          if item[:collection].blank?
-            item[:collection] = []
-          end
-          item[:collection] << session
-        end
-      end
-
-      if !has_find
-        logger.info "-----------------has not find---------------"
-        item[:starts_at] = starts_at
-        item[:collection] = []
-        item[:collection] << session
-      end
-
-      collection << item
-      logger.info "------collection: #{collection}"
-    end
-=end
-
   end
 
 end
