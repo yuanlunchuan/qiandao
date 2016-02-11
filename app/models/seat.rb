fed4847fa67477bfc6f5a0e541055efd676b3135
Yuan Lunchuan <799652013@qq.com>
2016-02-11 10:38:01 +0800

    1, Add seat.

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -5,5 +5,9 @@ class Seat < ActiveRecord::Base
 
   scope :session_is, ->(session) { where session_id: session.id }
   scope :seat_position_is, ->(row, col) { where "table_row=? AND table_col=?", row, col }
-
+  def to_hash
+    hash = {}
+    self.attributes.each { |k,v| hash[k] = v }
+    return hash
+  end
 end
