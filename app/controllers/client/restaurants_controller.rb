af6cbb9242b18f120420fd7495b395c3352091a3
Yuan Lunchuan <799652013@qq.com>
2016-02-12 09:16:35 +0800

    1, Add restaurant.

--- app/controllers/client/restaurants_controller.rb
+++ app/controllers/client/restaurants_controller.rb
@@ -4,6 +4,7 @@ class Client::RestaurantsController < ApplicationController
   before_action :load_attendee
   layout 'client'
   def show
-    @restaurant = current_event.restaurant
+    @restaurant = Restaurant.find_or_create_by(event: current_event)
+    #@restaurant = current_event.restaurant
   end
 end
