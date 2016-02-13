843fef33c71e47357c3681cd7ced3f96208ee341
Yuan Lunchuan <799652013@qq.com>
2016-02-13 14:24:53 +0800

    1, Add seller js.

--- app/assets/javascripts/seller_checkins_show.js
+++ app/assets/javascripts/seller_checkins_show.js
@@ -55,7 +55,8 @@ $(document).ready(function(){
         return;
       }
       var company = value.company ? value.company : '公司未知';
-      var tableItem = "<tr><td>"+value.name+"</td><td>"+company+"</td><td>"+'已签到'+"</td><td>"+"<td class='phone-button'><a href='tel:"+value.mobile+"'><span>一键拨号</span></a></td>";
+      var state = value.checked_in_at ? '已签到' : '未签到';
+      var tableItem = "<tr><td>"+value.name+"</td><td>"+company+"</td><td>"+state+"</td><td>"+"<td class='phone-button'><a href='tel:"+value.mobile+"'><span>一键拨号</span></a></td>";
       $('#attendee-table').append(tableItem);
     });
   }
