1424ea38bccc5fd35bcd0b68dafbf9d0c195990c
Yuan Lunchuan <799652013@qq.com>
2016-02-14 22:55:12 +0800

    1, Add pdf file.

--- app/pdfs/badge_pdf.rb
+++ app/pdfs/badge_pdf.rb
@@ -2,14 +2,13 @@ class BadgePdf < Prawn::Document
     def initialize(attendee, options={})
       pdf_options = {page_size: [154, 246], page_layout: :portrait, top_margin: 10, left_margin: 5, right_margin: 5, bottom_margin:0}
       if options[:background]
-        background = "#{Rails.root}/app/pdfs/badge_background.png"
+        background = "#{Rails.root}/app/pdfs/badge_background(2).png"
         pdf_options.merge!({background: background, background_scale: 0.25})
       end
 
       super(pdf_options)
-
       photo_path = attendee.avatar.path || "#{Rails.root}/app/pdfs/no_avatar.jpg"
-      image photo_path, width: 60, height: 60, position: :center, vposition: 53
+      image photo_path, width: 60, height: 60, position: :center, vposition: 80
 
       # stroke_color 50, 100, 0, 0
       # stroke_rectangle [0, 243], 153, 243
@@ -18,15 +17,17 @@ class BadgePdf < Prawn::Document
          'msyh' => { normal: "#{Rails.root}/app/fonts/msyh.ttf" }
       )
 
-      move_down 60
+      move_down 100
 
       font('msyh') do
         text attendee.name, align: :center, size: 14
         move_down 5
         text attendee.company, align: :center, size: 8
+        move_down 5
+        text attendee.category.try(:name), align: :center, size: 12
       end
 
-      image attendee.qrcode_image, width: 45, height: 45, position: :center, vposition: 152
+      #image attendee.qrcode_image, width: 45, height: 45, position: :center, vposition: 152
 
     end
 end
