bb8e16f7c4e1a2ac8da6feddcab725f7c5f0ab9a
Yuan Lunchuan <799652013@qq.com>
2016-02-14 17:58:59 +0800

    1, Add seats.

--- app/controllers/client/seats_controller.rb
+++ app/controllers/client/seats_controller.rb
@@ -8,12 +8,13 @@ class Client::SeatsController < ApplicationController
     attendee = Attendee.find_by(id: params[:attendee_id])
     render_not_found '没有找到对应嘉宾' and return if attendee.blank?
 
-    session = Session.find_by(id: params[:session_id])
-    render_not_found '没有找到对应日程' and return if session.blank?
+    # session = Session.find_by(id: params[:session_id])
+    # render_not_found '没有找到对应日程' and return if session.blank?
 
-    seats = Seat.attendee_seat_is(attendee, session)
+    # seats = Seat.attendee_seat_is(attendee, session)
 
-    render_not_found '没有找到座位信息' and return if seats.blank?
+    # render_not_found '没有找到座位信息' and return if seats.blank?
+    seats = Seat.attendee_seat_is(attendee, current_event.sessions.first)
 
     render_ok [ seats.first.to_hash ]
   end
