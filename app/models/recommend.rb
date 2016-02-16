015f53d31d718b0040587ff0ff5b9a433ba9792f
Yuan Lunchuan <799652013@qq.com>
2016-02-16 21:30:52 +0800

    1, remove phone.

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,6 +1,5 @@
 class Recommend < ActiveRecord::Base
   belongs_to :event
-  validates :phone_number, presence: true, length: { is: 11 }, numericality: { integer_only: true }, format: { with: /\A1[3-9]\d{9}\z/ }
   validates :category_name, presence: true
   validates :recommend_name, presence: true
   validates :address, presence: true
