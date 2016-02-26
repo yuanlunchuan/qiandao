da07cee205c76fd30ad6b4a6650efaf126aa4f81
Yuan Lunchuan <799652013@qq.com>
2016-02-26 23:56:33 +0800

    1, Update pdf file.

--- app/pdfs/badge_pdf.rb
+++ app/pdfs/badge_pdf.rb
@@ -1,36 +1,34 @@
 class BadgePdf < Prawn::Document
     def initialize(attendee, options={})
       pdf_options = {page_size: [154, 246], page_layout: :portrait, top_margin: 10, left_margin: 5, right_margin: 5, bottom_margin:0}
-      # if options[:background]
-      background = "#{Rails.root}/app/pdfs/red.png"
+      background = "#{Rails.root}/app/pdfs/guanzhou_red_background.png"
       if '经销商'==attendee.category.try(:name)
-        background = "#{Rails.root}/app/pdfs/badge_background_version2.png"
+        background = "#{Rails.root}/app/pdfs/guanzhou_blue_background.png"
       end
 
       pdf_options.merge!({background: background, background_scale: 0.25})
-      # end
 
       super(pdf_options)
       photo_path = attendee.avatar.path || "#{Rails.root}/app/pdfs/no_avatar.jpg"
-      image photo_path, width: 80, height: 80, position: :center, vposition: 80
+      image photo_path, width: 100, height: 100, position: :center, vposition: 50
 
-      # stroke_color 50, 100, 0, 0
-      # stroke_rectangle [0, 243], 153, 243
+      #stroke_color 225, 0, 0, 0
+      #stroke_rectangle [0, 243], 153, 243
 
       font_families.update(
          'msyh' => { normal: "#{Rails.root}/app/fonts/msyh.ttf" }
       )
 
-      move_down 88
+      move_down 58
 
       font('msyh') do
         text attendee.name, align: :center, size: 14
-        move_down 5
+        move_down 10
         company = ' '
         company = attendee.company if attendee.company.present?
         text company, align: :center, size: 8
-        move_down 17
-        text attendee.category.try(:name), align: :center, size: 9
+        move_down 20
+        text attendee.category.try(:name), align: :center, size: 9, color: "FFFFFF"
       end
 
       #image attendee.qrcode_image, width: 45, height: 45, position: :center, vposition: 152
