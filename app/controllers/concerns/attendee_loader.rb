28c9837f6b64c622b15b26b94096d847a3348246
Yuan Lunchuan <799652013@qq.com>
2016-07-10 18:20:20 +0800

    1, Add sessions page.

--- app/controllers/concerns/attendee_loader.rb
+++ app/controllers/concerns/attendee_loader.rb
@@ -7,7 +7,7 @@ module AttendeeLoader
     def load_attendee
       if cookies[:attendee_id].blank?
         cookies[:attendee_id] = nil
-        redirect_to new_client_session_path
+        redirect_to new_client_event_session_path(current_event)
       else
         attendee = Attendee.find_by(id: cookies[:attendee_id])
         if attendee.present?
@@ -15,11 +15,11 @@ module AttendeeLoader
             session[:attendee_id] = attendee.id
           else
             cookies[:attendee_id] = nil
-            redirect_to new_client_session_path
+            redirect_to new_client_event_session_path(current_event)
           end
         else
           cookies[:attendee_id] = nil
-          redirect_to new_client_session_path
+          redirect_to new_client_event_session_path(current_event)
         end
       end
     end
