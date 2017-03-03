e9278111ef7c9bf0bd4f09b04079d01ebc0ddac5
Yuan Lunchuan <799652013@qq.com>
2017-03-03 22:47:49 +0800

    1, Update attendee.

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -40,10 +40,10 @@ class Attendee < ActiveRecord::Base
   scope :printed, -> {where('printed_at IS NOT NULL')}
   scope :not_printed, -> {where('printed_at IS NULL')}
 
-  scope :has_photo, -> { where('photo_file_name IS NOT NULL') }
-  scope :does_not_have_photo, -> { where('photo_file_name IS NULL') }
+  scope :has_photo, -> { where('photo_updated_at IS NOT NULL') }
+  scope :does_not_have_photo, -> { where('photo_updated_at IS NULL') }
   scope :has_avatar, -> { where('avatar_file_name IS NOT NULL') }
-  scope :does_not_have_avatar, -> { where('avatar_file_name IS NULL') }
+  scope :does_not_have_avatar, -> { where('avatar_updated_at IS NULL') }
   scope :does_not_processed_avatar, -> { where('photo_updated_at IS NOT NULL AND avatar_updated_at IS NULL') }
 
   scope :category,  -> (category_id) { where(category_id: category_id) }
